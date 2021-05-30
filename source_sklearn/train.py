88082c6bc2256ee657a9a05a93e6100c012fc34f
funingtian <51334451+tian570@users.noreply.github.com>
2021-05-30 19:50:10 -0400

    Add files via upload

---
+++ source_sklearn/train.py
@@ -0,0 +1,77 @@
+from __future__ import print_function
+
+import argparse
+import os
+import pandas as pd
+
+# sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23. 
+# from sklearn.externals import joblib
+# Import joblib package directly
+import joblib
+
+## TODO: Import any additional libraries you need to define a model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer, fbeta_score
+
+# Provided model load function
+def model_fn(model_dir):
+    """Load model from the model_dir. This is the same model that is saved
+    in the main if statement.
+    """
+    print("Loading model.")
+    
+    # load using joblib
+    model = joblib.load(os.path.join(model_dir, "model.joblib"))
+    print("Done loading model.")
+    
+    return model
+
+
+## TODO: Complete the main code
+if __name__ == '__main__':
+    
+    # All of the model parameters and training parameters are sent as arguments
+    # when this script is executed, during a training job
+    
+    # Here we set up an argument parser to easily access the parameters
+    parser = argparse.ArgumentParser()
+
+    # SageMaker parameters, like the directories for training data and saving models; set automatically
+    # Do not need to change
+    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    
+    ## TODO: Add any additional arguments that you will need to pass into your model
+    
+    # args holds all passed-in arguments
+    args = parser.parse_args()
+
+    # Read in csv training file
+    training_dir = args.data_dir
+    train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
+
+    # Labels are in the first column
+    train_y = train_data.iloc[:,0]
+    train_x = train_data.iloc[:,1:]
+    
+    
+    ## --- Your code here --- ##
+    
+
+    ## TODO: Define a model
+    # generate a Gaussian classifer
+    clf = RandomForestClassifier(random_state=100)
+    parameters = {"max_depth": [2, None],
+                  "criterion": ["gini", "entropy"]}
+    
+    scorer = make_scorer(fbeta_score, beta=0.5)
+    grid_obj = GridSearchCV(clf, parameters, scoring=scorer)
+    
+    ## TODO: Train the model
+    grid_fit = grid_obj.fit(train_x, train_y)
+    best_clf = grid_fit.best_estimator_
+    
+    # Save the trained model
+    joblib.dump(best_clf, os.path.join(args.model_dir, "model.joblib"))
